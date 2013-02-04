9b230116f4dedb762db11a2ce77f55682997f169
James Womack <jamesw@amcoonline.net>
2013-02-03 22:47:02 -0800

    Initial Commit

---
+++ ABKit/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ABKit
+//
+//  Created by James Womack on 2/3/13.
+//  Copyright (c) 2013 James Womack. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
