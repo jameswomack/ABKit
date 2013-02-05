4662f0529408e0a1c181796ff4db3f5ca64ef2fb
James Womack <jamesw@amcoonline.net>
2013-02-04 23:29:46 -0800

    Alphabetization
    Alphabetization

--- ABKit/main.m
+++ ABKit/main.m
@@ -14,6 +14,10 @@ void      ABLog                     (NSString* format, ...);
 NSRange   ABRangeForString          (NSString* stringToExpressRangeFor);
 void      ABRangePointerForString   (NSRangePointer rangePointer, NSString* stringToExpressRangeFor);
 NSString* ABRangeStringForString    (NSString* stringToExpressRangeFor);
+NSArray*  ABArrayFromString         (NSString* stringToSplit);
+NSString* ABStringFromArray         (NSArray* arrayToJoinAsAString);
+NSArray*  ABAlphabetizedArray       (NSArray* arrayToAlphabetize);
+NSString* ABAlphabetizedString      (NSString* stringToAlphabetize);
 
 
 NSString* ABString(NSString* format, ...)
@@ -51,6 +55,45 @@ NSString* ABRangeStringForString(NSString* stringToExpressRangeFor)
     return NSStringFromRange(ABRangeForString(stringToExpressRangeFor));
 }
 
+NSArray* ABArrayFromString(NSString* stringToSplit)
+{
+    NSMutableArray* characterArray = NSMutableArray.new;
+    [stringToSplit enumerateSubstringsInRange:ABRangeForString(stringToSplit) options:(NSStringEnumerationByComposedCharacterSequences) usingBlock:^(NSString *substring, NSRange substringRange, NSRange enclosingRange, BOOL *stop) {
+        [characterArray addObject:substring];
+    }];
+    return characterArray;
+}
+
+NSString* ABStringFromArray(NSArray* arrayToJoinAsAString)
+{
+    NSMutableString* joinedArray = NSMutableString.new;
+    for (NSString* obj in arrayToJoinAsAString)
+    {
+        [joinedArray appendFormat:@"%@",obj];
+    }
+    return joinedArray;
+}
+
+NSArray* ABAlphabetizedArray(NSArray* arrayToAlphabetize)
+{
+    return [arrayToAlphabetize sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+        BOOL isPunct1 = [[NSCharacterSet punctuationCharacterSet] characterIsMember:[(NSString*)obj1 characterAtIndex:0]];
+        BOOL isPunct2 = [[NSCharacterSet punctuationCharacterSet] characterIsMember:[(NSString*)obj2 characterAtIndex:0]];
+        if (isPunct1 && !isPunct2) {
+            return NSOrderedAscending;
+        } else if (!isPunct1 && isPunct2) {
+            return NSOrderedDescending;
+        }
+        return [(NSString*)obj1 compare:obj2 options:NSDiacriticInsensitiveSearch|NSCaseInsensitiveSearch];
+    }];
+}
+
+NSString* ABAlphabetizedString(NSString* stringToAlphabetize)
+{
+    return ABStringFromArray(ABAlphabetizedArray(ABArrayFromString(stringToAlphabetize)));
+}
+
+
 
 
 int main(int argc, char *argv[])
@@ -60,6 +103,10 @@ int main(int argc, char *argv[])
     NSRange rangeToPointTo;
     ABRangePointerForString(&rangeToPointTo,@"Fore");
     ABLog(@"%@",NSStringFromRange(rangeToPointTo),nil);
+    
+    ABLog(@"%@",ABAlphabetizedArray(@[@"e",@"a",@"c"]),nil);
+    
+    ABLog(@"%@",ABAlphabetizedString(@"eca"),nil);
     
     return NSApplicationMain(argc, (const char **)argv);
 }
